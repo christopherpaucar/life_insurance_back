088a5e2e1db4e90f5e7c97b877727cbd2c7a436e
leninner <mazabandalenin180@gmail.com>
2025-05-30 07:36:21 -0500

    chore: update tests

--- src/common/utils/date.utils.ts
+++ src/common/utils/date.utils.ts
@@ -1,7 +1,7 @@
 export class DateUtils {
   static monthsBetween(startDate: Date, endDate: Date): number {
-    const start = new Date(startDate)
-    const end = new Date(endDate)
+    const start = startDate
+    const end = endDate
 
     const years = end.getFullYear() - start.getFullYear()
     const months = end.getMonth() - start.getMonth()
